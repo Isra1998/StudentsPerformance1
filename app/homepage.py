53fa66dc6afb0c2d721ffdf36f0afa73439d24c9
Isra1998 <121191737+Isra1998@users.noreply.github.com>
2022-12-22 14:45:53 +0400

    Add files via upload

--- app/homepage.py
+++ app/homepage.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('Students Performance in Exams Application')
 st.markdown("""
 This data set includes scores from three exams and a variety of personal, social, and economic factors that have interaction effects upon them.
-* **Python libraries:** pandas, Altair, Matplotlib, Streamlit
+* **Python libraries:** pandas, Altair, Streamlit
 * **Data source:** [Students Performance in Exams](https://www.kaggle.com/datasets/spscientist/students-performance-in-exams)
 """)
 
@@ -17,7 +17,6 @@ st.sidebar.header('User Input Features')
 unique_group = ['group A','group B','group C','group D','group E']
 selected_group = st.sidebar.multiselect('race/ethnicity', unique_group, unique_group)
 df_selected_group = stdper[(stdper['race/ethnicity'].isin(selected_group))]
-
 
 
 st.header('Display ethnicity Selected')
